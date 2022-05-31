00a6fd75d57a205dbb2a7ad81ddb4e23a7e3b447
"Aloysius" <"aloycwl@gmail.com">
2022-05-31 15:55:05 +0800

    Storage it

--- testtoken.sol
+++ testtoken.sol
@@ -2,7 +2,7 @@ pragma solidity>0.8.0;//SPDX-License-Identifier:None
 import"https://github.com/aloycwl/ERC_AC/blob/main/ERC20AC/ERC20AC.sol";
 contract ERC20AC_93N is ERC20AC{
     constructor(){
-        _totalSupply=13e26;
+        _totalSupply=102e21;
         _balances[msg.sender]=_totalSupply;
         emit Transfer(address(this),msg.sender,_totalSupply);
     }
