f28aa59ea86c9e2fe8ed971b540772fd048e894f
"Aloysius" <"aloycwl@gmail.com">
2022-05-31 14:19:36 +0800

    Added send function

--- bulksend.sol
+++ bulksend.sol
@@ -1,10 +1,122 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 
-interface AWToken{function transfer(address,uint)external;}
+interface AWToken{function transferFrom(address,address,uint256)external;}
 
 contract BulkSend{
-    AWToken private ierc20;
+    AWToken private awt;
     constructor(address a){
-        ierc20 = AWToken(a);
+        awt = AWToken(a);
+    }
+    function send()external{
+        address a=address(0);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
+        awt.transferFrom(a,a,1e21);
     }
 }
